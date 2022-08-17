bec74e183e15dafca802819b47e788b5472d89da
Moshiur Rahman <moshiur.rahman@ekkbaz.com>
2022-08-17 11:12:40 +0600

    Jwt movie list , user create

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { MoviesModule } from './movies/movies.module';
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:',
+    ),
+    UsersModule,
+    AuthModule,
+    MoviesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
